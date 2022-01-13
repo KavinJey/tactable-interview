882c994ac714e4d5e98877bc4763a67b6b20e21c
Kavin Jey <jeyakavin33@gmail.com>
2022-01-13 17:53:28 -0500

    feat: finished pagination query

--- components/articleCard.tsx
+++ components/articleCard.tsx
@@ -5,9 +5,15 @@ const ArticleCard: React.FC<Article> = (article: Article) => {
  const { title, description, updatedAt, createdAt, comments, authors  } = article;
 
  return (
-     <div>
+     <div className="flex flex-col">
         <h1> {title} </h1>
         <p> {description} </p>
+
+        <div className="details">
+
+          <p> Comments ({comments.length})  </p> 
+          <p> By {authors.map((author ) => { return `${author.name}\n` })}</p>
+        </div>    
      </div>
  )   
 }
